098b7386be4942d307b29125ff926c09aaba611e
Shane Witbeck <shane@digitalsanctum.com>
2023-08-26 09:03:36 -0700

    feat: add job notes, update db scripts

--- database/migrations/V2__Create_companies.sql
+++
@@ -1,3 +0,0 @@
-insert into jobosint.jobosint.company(name, url) values('Google', 'https://google.com');
-insert into jobosint.jobosint.company(name, url) values('Microsoft', 'https://microsoft.com');
-insert into jobosint.jobosint.company(name, url) values('Facebook', 'https://facebook.com');
