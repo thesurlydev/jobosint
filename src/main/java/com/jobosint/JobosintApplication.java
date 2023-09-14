02b013519e1a03245d80bd069a4cd61f6ea7f795
Shane Witbeck <shane@digitalsanctum.com>
2023-09-14 10:02:40 -0700

    feat: add method perf logging

--- src/main/java/com/jobosint/JobosintApplication.java
+++ src/main/java/com/jobosint/JobosintApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableFeignClients
+@EnableAspectJAutoProxy
 @ConfigurationPropertiesScan("com.jobosint.config")
 @SpringBootApplication
 public class JobosintApplication {
