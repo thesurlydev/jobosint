b5c0a0de0550548fba12643411bf2f2dae4882de
Shane Witbeck <switbeck@mediafly.com>
2024-02-22 09:12:10 -0800

    feat: Switch to pgvector database for Spring AI vector store

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,21 @@
 -- drop table if exists job;
 -- drop table if exists company;
 
+-- start Spring AI vector store
+CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS hstore;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS vector_store (
+                                            id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+                                            content text,
+                                            metadata json,
+                                            embedding vector(1536)
+);
+
+CREATE INDEX ON vector_store USING HNSW (embedding vector_cosine_ops);
+-- end Spring AI vector store
+
 create table if not exists company
 (
     id               uuid primary key default gen_random_uuid(),
