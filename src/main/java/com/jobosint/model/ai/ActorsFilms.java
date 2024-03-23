ac6f64a53c6bb1cbe211082bc0e688f611ef6f27
Shane Witbeck <switbeck@mediafly.com>
2024-03-23 09:41:51 -0700

    chore: Remove example code

--- src/main/java/com/jobosint/model/ai/ActorsFilms.java
+++
@@ -1,7 +0,0 @@
-package com.jobosint.model.ai;
-
-import java.util.List;
-
-
-public record ActorsFilms(String actor, List<Movie> movies) {
-}
--- src/main/java/com/jobosint/model/ai/Movie.java
+++
@@ -1,4 +0,0 @@
-package com.jobosint.model.ai;
-
-public record Movie(String title, Integer yearReleased) {
-}
