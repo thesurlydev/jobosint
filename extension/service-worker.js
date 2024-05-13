2df6e39df98cf3bc5347b0f4c64912691c297cc0
Shane Witbeck <shane@surly.dev>
2024-05-13 10:56:25 -0700

    feat: Multiple origin handling

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,17 +1,18 @@
-console.log('service-worker.js loaded');
-
 const LINKEDIN_ORIGIN = 'https://www.linkedin.com';
+const LINKEDIN_ORIGIN2 = 'https://linkedin.com';
 
 // Allows users to open the side panel by clicking on the action toolbar icon
 chrome.sidePanel
-    .setPanelBehavior({openPanelOnActionClick: true})
+    .setPanelBehavior({
+        openPanelOnActionClick: true
+    })
     .catch((error) => console.error(error));
 
 chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
     if (!tab.url) return;
     const url = new URL(tab.url);
     // Enables the side panel on linkedin.com
-    if (url.origin === LINKEDIN_ORIGIN) {
+    if (url.origin === LINKEDIN_ORIGIN || url.origin === LINKEDIN_ORIGIN2) {
         await chrome.sidePanel.setOptions({
             tabId,
             path: 'sidepanel.html',
